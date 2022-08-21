98a874ea041bf7952ad5d297672e7a5ec5837a5a
EC2 Default User <shion@example.com>
2022-08-21 20:11:58 +0000

    passed_by_value.phpを書いた

---
+++ passed_by_value.php
@@ -0,0 +1,17 @@
+<?php
+//関数の宣言
+function myFunc(int $val)
+{
+    //$valの値を２倍にする
+    $val *= 2;//"$val = $val * 2"と同じ意味を持つ、複合演算子という書き方
+    //値を確認する
+    echo "in myFunc, val is {$val} \n";
+    
+}
+
+//変数への代入
+$i = 10;
+//関数の実行
+myFunc($i);
+//変数　i の中身の確認
+echo "i is {$i} \n";
